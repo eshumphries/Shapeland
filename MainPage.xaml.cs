17d6ef7c2e4d0215aed23545ca8f9171a7a77dee
eshumphries <eshumphries@outlook.com>
2025-07-02 10:38:36 -0400

    Added the code to make the page with page elements resize according to screen size

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Shapeland
 {
@@ -11,9 +12,18 @@ namespace Shapeland
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
 
-            // Enter construction logic here...
+            DisplayScrollViewer.SizeChanged += DisplayScrollViewer_SizeChanged;
+        }
+        private void DisplayScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double scale = Math.Min(e.NewSize.Width / 800, e.NewSize.Height / 600);
+            DisplayGrid.RenderTransform = new ScaleTransform
+            {
+                ScaleX = scale * 1.8,
+                ScaleY = scale * 1.8
+            };
         }
     }
 }
